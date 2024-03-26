d097e47661f78c8f47fcd534fa843303aee7ec41
hayrunnisayilbur <145451185+hayrunnisayilbur@users.noreply.github.com>
2024-03-26 14:05:11 +0300

    Add files via upload

---
+++ veritabani2-847/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace veritabani2_847
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgwFilmler = new System.Windows.Forms.DataGridView();
+            this.txtFilmAdi = new System.Windows.Forms.TextBox();
+            this.txtFilmYonetmen = new System.Windows.Forms.TextBox();
+            this.txtYil = new System.Windows.Forms.TextBox();
+            this.txtFilmSure = new System.Windows.Forms.TextBox();
+            this.btnGuncelle = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgwFilmler)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgwFilmler
+            // 
+            this.dgwFilmler.AllowUserToAddRows = false;
+            this.dgwFilmler.AllowUserToDeleteRows = false;
+            this.dgwFilmler.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgwFilmler.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgwFilmler.Location = new System.Drawing.Point(12, 288);
+            this.dgwFilmler.Name = "dgwFilmler";
+            this.dgwFilmler.ReadOnly = true;
+            this.dgwFilmler.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgwFilmler.Size = new System.Drawing.Size(776, 150);
+            this.dgwFilmler.TabIndex = 0;
+            this.dgwFilmler.CellEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgwFilmler_CellEnter);
+            // 
+            // txtFilmAdi
+            // 
+            this.txtFilmAdi.Location = new System.Drawing.Point(162, 29);
+            this.txtFilmAdi.Name = "txtFilmAdi";
+            this.txtFilmAdi.Size = new System.Drawing.Size(100, 20);
+            this.txtFilmAdi.TabIndex = 1;
+            // 
+            // txtFilmYonetmen
+            // 
+            this.txtFilmYonetmen.Location = new System.Drawing.Point(162, 64);
+            this.txtFilmYonetmen.Name = "txtFilmYonetmen";
+            this.txtFilmYonetmen.Size = new System.Drawing.Size(100, 20);
+            this.txtFilmYonetmen.TabIndex = 2;
+            // 
+            // txtYil
+            // 
+            this.txtYil.Location = new System.Drawing.Point(162, 101);
+            this.txtYil.Name = "txtYil";
+            this.txtYil.Size = new System.Drawing.Size(100, 20);
+            this.txtYil.TabIndex = 3;
+            // 
+            // txtFilmSure
+            // 
+            this.txtFilmSure.Location = new System.Drawing.Point(162, 137);
+            this.txtFilmSure.Name = "txtFilmSure";
+            this.txtFilmSure.Size = new System.Drawing.Size(100, 20);
+            this.txtFilmSure.TabIndex = 4;
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnGuncelle.Location = new System.Drawing.Point(282, 101);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(130, 56);
+            this.btnGuncelle.TabIndex = 5;
+            this.btnGuncelle.Text = "Güncelle";
+            this.btnGuncelle.UseVisualStyleBackColor = true;
+            this.btnGuncelle.Click += new System.EventHandler(this.btnGuncelle_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(70, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Film Adı:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(70, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Yönetmen:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(70, 108);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Çekim Yılı:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(70, 140);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "FilmSuresi:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnGuncelle);
+            this.Controls.Add(this.txtFilmSure);
+            this.Controls.Add(this.txtYil);
+            this.Controls.Add(this.txtFilmYonetmen);
+            this.Controls.Add(this.txtFilmAdi);
+            this.Controls.Add(this.dgwFilmler);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgwFilmler)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgwFilmler;
+        private System.Windows.Forms.TextBox txtFilmAdi;
+        private System.Windows.Forms.TextBox txtFilmYonetmen;
+        private System.Windows.Forms.TextBox txtYil;
+        private System.Windows.Forms.TextBox txtFilmSure;
+        private System.Windows.Forms.Button btnGuncelle;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ veritabani2-847/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using Mysqlx.Crud;
+
+namespace veritabani2_847
+{
+    public partial class Form1 : Form
+    {
+        string baglanti = "Server=localhost;Database=film_arsiv;Uid=root;Pwd='';";
+        DataTable dt;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            using (MySqlConnection baglan = new MySqlConnection(baglanti))
+            {
+                baglan.Open();
+                string sorgu = "select * from filmler;";
+                MySqlCommand cmd = new MySqlCommand(sorgu, baglan);
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+
+                dt = new DataTable();
+                da.Fill(dt);
+
+                dgwFilmler.DataSource = dt;
+            }
+        }
+
+        private void dgwFilmler_CellEnter(object sender, DataGridViewCellEventArgs e)
+        {
+            txtFilmAdi.Text = dgwFilmler.CurrentRow.Cells[1].Value.ToString();
+            txtFilmYonetmen.Text = dgwFilmler.CurrentRow.Cells[2].Value.ToString();
+            txtYil.Text = dgwFilmler.CurrentRow.Cells[3].Value.ToString();
+            txtFilmSure.Text = dgwFilmler.CurrentRow.Cells[4].Value.ToString();
+        }
+
+        private void btnGuncelle_Click(object sender, EventArgs e)
+        {
+            if(dgwFilmler.SelectedRows.Count > 0)
+            {
+                using (MySqlConnection baglan = new MySqlConnection(baglanti))
+                {
+                    baglan.Open();
+                    string sorgu = "UPDATE filmler " +
+                                   "SET film_ad =@film_ad, yonetmen =@yonetmen, sure =@sure, yil= @yil" +
+                                   " WHERE film_id=@film_id";
+                    MySqlCommand cmd = new MySqlCommand(sorgu, baglan);
+
+                    cmd.Parameters.AddWithValue("@film_ad", txtFilmAdi.Text);
+                    cmd.Parameters.AddWithValue("@yonetmen", txtFilmYonetmen.Text);
+                    cmd.Parameters.AddWithValue("@sure", txtFilmSure.Text);
+                    cmd.Parameters.AddWithValue("@yil", txtYil.Text);
+                    cmd.Parameters.AddWithValue("@film_id", dgwFilmler.CurrentRow.Cells[0].Value.ToString());
+                    cmd.ExecuteNonQuery();
+
+                    dgwGuncelle();
+                }
+            }
+        }
+
+        
+        void dgwGuncelle()
+        {
+            using (MySqlConnection baglan = new MySqlConnection(baglanti))
+            {
+                baglan.Open();
+                string sorgu = "select * from filmler;";
+                MySqlCommand cmd = new MySqlCommand(sorgu, baglan);
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+
+                dt = new DataTable();
+                da.Fill(dt);
+
+                dgwFilmler.DataSource = dt;
+            }
+        }
+    }
+}
